39f20aaad1ca970ffeb466317eba4e8b90e3fad6
Albin Rodriguez <albinmrodriguez@gmail.com>
2022-04-22 17:33:09 -0400

    arregle/agregue la funcion de cerrar ventana de menu cuando clickean fuera de ella

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+var container = document.querySelector('.nav');
+
+
+document.querySelector(".menu-btn__icon").addEventListener("click", show_menu); 
+document.querySelector(".nav__icon").addEventListener("click", hide_menu);
+
+function show_menu(e){
+    container.classList.toggle("show");
+    document.querySelector(".nav__close").style.display = "block";
+    e.stopPropagation();
+}
+
+function hide_menu(){
+    container.classList.remove("show");
+    document.querySelector(".nav__close").style.display = "none";
+}
+
+//hide menu when click outside
+// document.addEventListener('click', (event) => {
+//     const withinBoundaries = event.composedPath().includes(container)
+  
+//     if (withinBoundaries && container.classList.contains("show")) {
+//         return;
+//     } 
+//     else if(!withinBoundaries && container.classList.contains("show")){
+//         container.classList.remove("show");
+//         document.querySelector(".nav__close").style.display = "none";
+//     }
+//   });
+document.addEventListener("click", function(event) {
+const isClosest = event.target.closest(".nav");
+    
+if (!isClosest && container.classList.contains("show")) {
+    container.classList.remove("show");
+    document.querySelector(".nav__close").style.display = "none";
+}
+});
+
+
+
+
